31875e8cb6d432edec2818ba2710a57ebd0b7b5c
nemo <18136225579@163.com>
2022-01-19 10:09:28 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+console.log("Hello World!");
+
+export const obj = {
+    a: 1
+}
+
+export class Service {
+    alert() {
+        window.alert('test')
+    }
+}
+
+function test() {
+    alert(1)
+}
+
+test()
